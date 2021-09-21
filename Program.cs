83d3b79170ce820b2498f985ac969d24791b7a0e
exiw <51192517+exiw@users.noreply.github.com>
2021-09-21 20:05:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyukHafKucukHarf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Lütfen kelimeyi giriniz");
+            string kelime = Console.ReadLine();
+            string buyukharf = kelime.ToUpper();
+            string kucukharf = kelime.ToLower();
+            Console.WriteLine(" Büyük Harf :" + buyukharf);
+            Console.WriteLine(" Küçük Harf :" + kucukharf);
+            Console.ReadKey();
+        }
+    }
+}
